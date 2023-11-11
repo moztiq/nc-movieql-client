9dbaa893ce62bbf85a63050945e38fe2c89bcda3
Seongki Kim <moztiq@gmail.com>
2023-11-11 12:04:19 +0900

    ApolloProvider

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,29 @@
+import { gql, useApolloClient } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
 export default function Movies() {
-  return <div></div>;
+  const client = useApolloClient();
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              title
+              id
+            }
+          }
+        `,
+      })
+      .then((result) => setMovies(result.data.allMovies));
+  }, [client]);
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
 }

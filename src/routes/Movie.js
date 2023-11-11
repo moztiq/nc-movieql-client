3880020e49e13c0c04b19d9f4c98da2f014c2aca
Seongki Kim <moztiq@gmail.com>
2023-11-11 13:16:10 +0900

    local only fields

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,16 +1,58 @@
 import { useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
 
 const GET_MOVIE = gql`
   query getMovie($movieId: ID!) {
     movie(id: $movieId) {
       id
-      genres
       title
-      small_cover_image
+      medium_cover_image
+      rating
+      isLiked @client
     }
   }
 `;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Image = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
+
 export default function Movie() {
   const { id } = useParams();
   const { data, loading, error } = useQuery(GET_MOVIE, {
@@ -25,17 +67,13 @@ export default function Movie() {
     return <h1>Could not fetch :(</h1>;
   }
   return (
-    <>
-      <img src={data.movie.small_cover_image} />
-      <ul>
-        <li>{data.movie.id}</li>
-        <li>{data.movie.title}</li>
-        <li>
-          {data.movie.genres?.map((genre) => (
-            <span key={genre}>{genre}</span>
-          ))}
-        </li>
-      </ul>
-    </>
+    <Container>
+      <Column>
+        <Title>{loading ? 'Loading...' : `${data.movie?.title}`}</Title>
+        <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+        <button>{data.movie.isLike ? 'Unlike' : 'Like'}</button>
+      </Column>
+      <Image bg={data?.movie?.medium_cover_image} />
+    </Container>
   );
 }
